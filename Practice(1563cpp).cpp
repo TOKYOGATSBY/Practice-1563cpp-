824206317433d8602a35f14fde5eed1d96c7967e
Grigorii Kolbeev <gold_grigorii@mail.ru>
2025-04-01 11:53:52 +0300

    version 1.0

--- Practice(1563cpp).cpp
+++ Practice(1563cpp).cpp
@@ -1,7 +1,28 @@
 ﻿#include <iostream>
+#include <unordered_set>
+#include <string>
+using namespace std;
 
-int main()
-{
-    
+int main() {
+    int N;
+    cin >> N;
+    cin.ignore();
+
+    unordered_set<string> visited;
+    int bayan_count = 0;
+
+    for (int i = 0; i < N; ++i) {
+        string store;
+        getline(cin, store);
+
+        if (visited.count(store)) {
+            bayan_count++;
+        }
+        else {
+            visited.insert(store);
+        }
+    }
+
+    cout << bayan_count << endl;
+    return 0;
 }
-
